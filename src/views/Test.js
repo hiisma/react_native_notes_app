b8e134fe26be0f96afc3217f2688b9a39325fce2
Ismail <isma.ao.inf@gmail.com>
2022-11-26 19:02:15 +0100

    Created first view

---
+++ src/views/Test.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Test extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This a test page</Text>
+            </View>
+        );
+    }
+}
